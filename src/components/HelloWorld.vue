cdbf4f7fa4cfef27806bf57e3985f4e17af2d8c3
shiyu <shiyu.now@gmail.com>
2018-04-24 22:06:39 +0800

    vue-cli复习 todolist小练习

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="hello">
+    <h1>{{msg}}</h1>
+    <input type="text" v-model="inputValue"/>
+    <button @click="handleSubmit">提交</button>
+    <ul>
+      <TodoList
+        v-for="(item, index) of list"
+        :key="index"
+        :content="item"
+        :index="index"
+        @delete="handleDelete"
+      ></TodoList>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+import TodoList from './TodoList'
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    TodoList
+  },
+  data () {
+    return {
+      msg: 'This is vue-cli todoList',
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleSubmit () {
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete (index) {
+      this.list.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="todo-list">
+    <li @click="handleDelete" title="点击删除">{{content}}</li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: ['content', 'index'],
+  methods: {
+    handleDelete () {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.todo-list{
+  margin-top: 10px;
+}
+  .todo-list li{
+    cursor: pointer
+  }
+</style>
